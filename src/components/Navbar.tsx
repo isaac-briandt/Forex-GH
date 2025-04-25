0e1c182e5efe323441935fa76b2912e9986120d9
isaac <isaacbriandt10@gmail.com>
2025-04-25 01:35:39 +0000

    make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import { Transition } from "@headlessui/react";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 export default function Navbar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
+
   return (
     <>
-      <div className="h-[102px] flex justify-center items-center bg-transparent px-20 py-6">
+      <div className="h-[102px] relative w-full flex justify-between items-center bg-transparent px-20 py-6">
         <img
           className="object-cover mr-6"
           src="/images/forexgh-logo.svg"
@@ -9,7 +19,7 @@ export default function Navbar() {
           height={48}
           alt="logo"
         />
-        <ul className="flex space-x-8 ml-auto text-white font-medium text-lg">
+        <ul className="md:flex hidden space-x-8 ml-auto text-white font-medium text-lg">
           <li className="py-4 px-2">
             <a href="">Exchange Rate</a>
           </li>
@@ -20,6 +30,36 @@ export default function Navbar() {
             <a href="">News</a>
           </li>
         </ul>
+        <div className="md:hidden block">
+          <div>
+            {showDropdown ? (
+              <X onClick={handleDropdown} className="text-white size-8 cursor-pointer" />
+            ) : (
+              <Menu onClick={handleDropdown} className="text-white size-8 cursor-pointer" />
+            )}
+          </div>
+          <Transition
+            show={showDropdown}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <ul className="absolute transition ease-out duration-100 right-0 left-0 inset-x-1/2 mt-5 w-full min-h-screen rounded-md bg-white/97 shadow-lg ring-black ring-opacity-5 focus:outline-none">
+              <li className="py-4 px-2">
+                <a href="">Exchange Rate</a>
+              </li>
+              <li className="py-4 px-2">
+                <a href="">Converter</a>
+              </li>
+              <li className="py-4 px-2">
+                <a href="">News</a>
+              </li>
+            </ul>
+          </Transition>
+        </div>
       </div>
     </>
   );
